ed9344ec7becd06f217bfd97a140fa110a53490d
amineHorseman <bendahmane.amine@gmail.com>
2016-09-07 18:13:41 +0900

    bugfix: reset/empty the graph before building the model. If we don't reset it, the new ops will be added to the old ones if we re-execute train() function without restarting the kernel

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 
+import tensorflow as tf
 from tflearn import DNN
 import time
 import argparse
@@ -13,42 +14,43 @@ def train(optimizer=HYPERPARAMS.optimizer, learning_rate=HYPERPARAMS.learning_ra
         print "loading dataset " + DATASET.name + "..."
         data, validation = load_data(validation=True)
 
-        print "building model..."
-        network = build_model(optimizer, learning_rate, keep_prob, learning_rate_decay, decay_step)
-        model = DNN(network, tensorboard_dir=TRAINING.logs_dir, 
-                tensorboard_verbose=0, checkpoint_path=TRAINING.checkpoint_dir,
-                max_checkpoints=TRAINING.max_checkpoints)
+        with tf.Graph().as_default():
+                print "building model..."
+                network = build_model(optimizer, learning_rate, keep_prob, learning_rate_decay, decay_step)
+                model = DNN(network, tensorboard_dir=TRAINING.logs_dir, 
+                        tensorboard_verbose=0, checkpoint_path=TRAINING.checkpoint_dir,
+                        max_checkpoints=TRAINING.max_checkpoints)
 
-        #tflearn.config.init_graph(seed=None, log_device=False, num_cores=6)
+                #tflearn.config.init_graph(seed=None, log_device=False, num_cores=6)
 
-        print "start training..."
-        print "  - learning_rate = {}".format(learning_rate)
-        print "  - learning_rate_decay = {}".format(learning_rate_decay)
-        print "  - keep_prob = {}".format(keep_prob)
-        print "  - epochs = {}".format(TRAINING.epochs)
-        start_time = time.time()
-        """model.fit({'input1': data['X']}, {'output': data['Y']}, n_epoch=TRAINING.epochs, 
-                validation_set=({'input': validation['X']}, {'output': validation['Y']}),
-                snapshot_step=TRAINING.snapshot_step, show_metric=TRAINING.vizualize,
-                batch_size=TRAINING.batch_size)
-        """
-        model.fit([data['X'], data['X2']], data['Y'],
-                validation_set=([validation['X'], validation['X2']], validation['Y']),
-                n_epoch=TRAINING.epochs,
-                show_metric=TRAINING.vizualize,
-                batch_size=TRAINING.batch_size)
-        training_time = time.time() - start_time
-        print "training time = {0:.1f} sec".format(training_time)
+                print "start training..."
+                print "  - learning_rate = {}".format(learning_rate)
+                print "  - learning_rate_decay = {}".format(learning_rate_decay)
+                print "  - keep_prob = {}".format(keep_prob)
+                print "  - epochs = {}".format(TRAINING.epochs)
+                start_time = time.time()
+                """model.fit({'input1': data['X']}, {'output': data['Y']}, n_epoch=TRAINING.epochs, 
+                        validation_set=({'input': validation['X']}, {'output': validation['Y']}),
+                        snapshot_step=TRAINING.snapshot_step, show_metric=TRAINING.vizualize,
+                        batch_size=TRAINING.batch_size)
+                """
+                model.fit([data['X'], data['X2']], data['Y'],
+                        validation_set=([validation['X'], validation['X2']], validation['Y']),
+                        n_epoch=TRAINING.epochs,
+                        show_metric=TRAINING.vizualize,
+                        batch_size=TRAINING.batch_size)
+                training_time = time.time() - start_time
+                print "training time = {0:.1f} sec".format(training_time)
 
-        if TRAINING.save_model:
-                print "saving model..."
-                model.save(TRAINING.save_model_path)
+                if TRAINING.save_model:
+                        print "saving model..."
+                        model.save(TRAINING.save_model_path)
 
-        print "evaluating..."
-        validation_accuracy = evaluate(model, validation['X'], validation['X2'], validation['Y'])
-        print "  - accuracy = {0:.1f}".format(validation_accuracy*100)
+                print "evaluating..."
+                validation_accuracy = evaluate(model, validation['X'], validation['X2'], validation['Y'])
+                print "  - accuracy = {0:.1f}".format(validation_accuracy*100)
 
-        return validation_accuracy
+                return validation_accuracy
 
 def evaluate(model, X, X2, Y):
         accuracy = model.evaluate([X, X2], Y)
